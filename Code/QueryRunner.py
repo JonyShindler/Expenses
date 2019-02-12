bf933d35811ceb54f355ff1d202e8c1d60fb7b76
JShindler <jonathanshindler@hotmail.com>
2019-02-12 21:02:05 +0000

    Test commit

--- Code/QueryRunner.py
+++ Code/QueryRunner.py
@@ -2,5 +2,5 @@ from DataWriter import createExpenseLine
 from DataWriter import writeExpenseToFile
 
 writeExpenseToFile(createExpenseLine(100, 'Jemma', 'blobby'))
-
+# TODO get this to work
 df = pd.read_csv("testWritingExpenses.csv", encoding="ISO-8859-1")
